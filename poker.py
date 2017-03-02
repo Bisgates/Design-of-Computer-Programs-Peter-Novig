7caefce5907c1d2a0333c3bfee247731281a29ad
Han Jialu <bisgates@outlook.com>
2017-03-02 16:59:10 +0800

    .

--- poker.py
+++ poker.py
@@ -23,7 +23,7 @@ def hand_rank(hand):
 		return(2,two_pairs(ranks),ranks)
 	elif(kind(2,ranks)):
 		return(1,kind(2,ranks),ranks)
-	elif:
+	else:
 		return(0,ranks)
 
 def card_ranks(cards):
@@ -35,45 +35,51 @@ def card_ranks(cards):
 
 def straight(ranks):
 	# Is this hand straight [9C,8D,7H,6S,5D] => 9
-
-	return ans
-
-def kind(n,ranks):
-	# Is there n kinds (2,[10,10,2,3,5]) => 10
-
-	return 
+	return (ranks[0]-ranks[-1]==4) and len(set(ranks))==5
 
 def flush(hand):
 	# Is this a fulsh [D,D,D,D,D] => 1
+	h = [s for r,s in hand]
+	return len(set(h))==1
 
-	return
+def kind(n,ranks):
+	# Is there n kinds (2,[10,10,5,3,2]) => 10
+	for r in ranks:
+		if(ranks.count(r)==n): return r
+	return None
 
 def two_pairs(ranks):
-	# is any pair there [10,10,8,8,4] => (10,8) / [3,4,5,6,7] => 0
-
-	return
+	# is any pair there [10,10,8,8,4] => (10,8) / [3,4,5,6,7] => None
+	high = kind(2,ranks)
+	lower = kind(2,sorted(ranks))
+	if(high and high!=lower):
+		return (high,lower)
+	return None
 
 def test():
     # To be a good programmer you must be a good tester
     sf = "6C 7C 8C 9C TC".split()
     fk = "9D 9H 9S 9C 7D".split()
     fh = "TD TC TH 7C 7D".split()
+    tp = "3D 3C 4D 4C 8D".split()
     assert card_ranks(sf) == [10,9,8,7,6]
     assert card_ranks(fk) == [9,9,9,9,7]
     assert card_ranks(fh) == [10,10,10,7,7]
-
     assert straight([9,8,7,6,5]) == True
     assert straight([9,8,7,6,4]) == False
     assert flush(sf) == True
     assert flush(fk) == False
-
-
+    assert kind(4,card_ranks(fk)) == 9
+    assert kind(3,card_ranks(fk)) == None
+    assert kind(2,card_ranks(fk)) == None
+    assert kind(1,card_ranks(fk)) == 7
+    assert two_pairs(card_ranks(tp)) == (4,3)
+    assert two_pairs(card_ranks(sf)) == None
     assert poker([sf,fk,fh]) == sf
     assert poker([fk,fh]) == fk
     assert poker([fh,fh]) == fh
     assert poker([fh]) == fh
     assert poker([sf]+[fh]*99) == sf
-    # hand_rank test
     assert hand_rank(sf) == (8,10)
     assert hand_rank(fk) == (7,9,7)
     assert hand_rank(fh) == (6,10,7)
@@ -91,4 +97,4 @@ def test():
     assert hand_rank(t0) == (0,card_ranks(t0))
     print("Tests pass")
 
- print(test())
+test()
